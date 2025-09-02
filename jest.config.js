e7e26bae4a15c75c38150f03cd6b1fce78b638ee
bonsai <onsen.bonsai@gmail.com>
2025-09-02 14:50:52 +0900

    Update test environment to node

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'node', // Changed from jsdom to node
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
